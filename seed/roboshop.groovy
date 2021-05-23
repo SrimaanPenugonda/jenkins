e03e760d4deba990501d4426f5c358536b50e839
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-05-23 10:40:43 +0530

    added ALB

--- seed/roboshop.groovy
+++ seed/roboshop.groovy
@@ -24,3 +24,24 @@ pipelineJob('Roboshop-Releases/VPC') {
         }
     }
 }
+
+pipelineJob('Roboshop-Releases/ALB') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/SrimaanPenugonda/terraform-alb.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
