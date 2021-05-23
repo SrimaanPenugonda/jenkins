799de5ec44d159e171669082b21bc6f2e44d8300
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-05-23 11:47:27 +0530

    frontend added

--- seed/roboshop.groovy
+++ seed/roboshop.groovy
@@ -171,3 +171,24 @@ pipelineJob('Roboshop-Releases/SHIPPING') {
         }
     }
 }
+
+pipelineJob('Roboshop-Releases/FRONTEND') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/SrimaanPenugonda/terraform-frontend.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
